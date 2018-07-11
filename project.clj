1f1e18ad03f791212834e89b72816f49e4b74f4d
Glenn Jahnke <gleenn@gmail.com>
2018-07-11 11:04:37 -0500

    cleanup project file

--- project.clj
+++ project.clj
@@ -1,11 +1,6 @@
 (defproject cortex-toy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :main cortex-toy.core
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [thinktopic/cortex "0.9.22"]
                  [thinktopic/experiment "0.9.22"]
-                 [org.clojure/data.csv "0.1.3"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]}})
+                 [org.clojure/data.csv "0.1.3"]])
